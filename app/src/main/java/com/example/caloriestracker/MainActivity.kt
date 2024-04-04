febfdc20929d9ab6735650c0049c09abdb3cbe20
Alaa.Elfeshawy <alaa.elfeshawy@linkdev.com>
2024-04-04 15:02:38 +0200

    implement navigation

--- app/src/main/java/com/example/caloriestracker/MainActivity.kt
+++ app/src/main/java/com/example/caloriestracker/MainActivity.kt
@@ -7,7 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.caloriestracker.navigation.navigateTo
 import com.example.caloriestracker.ui.theme.CaloriesTrackerTheme
+import com.example.core.navigation.Route
 import com.example.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,7 +27,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    WelcomeScreen()
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = Route.WELCOME ){
+                        composable(Route.WELCOME){
+                            WelcomeScreen{
+                                navController.navigateTo(it)
+                            }
+                        }
+                        composable(Route.AGE){
+                        }
+                        composable(Route.ACTIVITY){
+                        }
+                        composable(Route.GENDER){
+                        }
+                        composable(Route.GOAL){
+                        }
+                        composable(Route.WEIGHT){
+                        }
+                        composable(Route.NUTRIENT_GOAL){
+                        }
+                        composable(Route.SEARCH){
+                        }
+                        composable(Route.TRACKER_OVERVIEW){
+                        }
+                        composable(Route.HEIGHT){
+                        }
+                    }
                 }
             }
         }
